8b9ac930824efcb0e34a33c2c31f646216938ff2
A-W-Smith <aws2411@gmail.com>
2015-02-21 20:42:57 +0000

    So much git

--- name_gen.py
+++ name_gen.py
@@ -3,5 +3,8 @@ def startup_words():
 	buzznouns = ['synergy', 'adhesion','cohesion','impact','globalization']
 	computer = ['cache','api','mainframe','firewall','server','cloud','RAM','online']
 	company = ['.com','enterprises','ltd','international','incorporated','consolidated']
-	x = random.randint(0,5)
-	return buzzadj[x] + ' ' + buzznouns[x] + ' ' + company[x]
+	x = random.randint(0,3)
+	y = random.randint(0,6)
+	z = random.randint(0,9)
+	output = [ buzzadj[y] + company[x], buzzadj[z] + buzznouns[x], computer[y] + company[x], buzzadj[z] + computer[y] + company[x]]
+	return output[x]
