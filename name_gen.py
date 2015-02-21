dd4fe6623282f2193dc94fe4dc4f45fa294776fa
A-W-Smith <aws2411@gmail.com>
2015-02-21 20:37:05 +0000

    I can git now

--- name_gen.py
+++ name_gen.py
@@ -1,5 +1,7 @@
-def words():
-	buzzwords = ['grass-roots', 'synergy' , 'proactive','adhesive','cohesive','practical','globalization','robust','sustainabale','impact','functional','transformative','dynamic']
+def startup_words():
+	buzzadj = ['grass-roots', 'synergistic' , 'proactive','adhesive','cohesive','practical','robust','sustainabale','functional','transformative','dynamic']
+	buzznouns = ['synergy', 'adhesion','cohesion','impact','globalization']
 	computer = ['cache','api','mainframe','firewall','server','cloud','RAM','online']
 	company = ['.com','enterprises','ltd','international','incorporated','consolidated']
-	
+	x = random.randint(0,5)
+	return buzzadj[x] + ' ' + buzznouns[x] + ' ' + company[x]
