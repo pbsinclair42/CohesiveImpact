199c25c66568e1e6246c32d9275bcf2a54162124
Paul Sinclair <bobsmith0030+git2@gmail.com>
2015-02-21 21:29:54 +0000

    Reduced probability of AVL appearing

--- name_gen.py
+++ name_gen.py
@@ -6,8 +6,8 @@ def startup_words():
 	buzznouns = ['Synergy', 'Adhesion','Cohesion','Impact','Globalization','Solutions','Administration','Management','Paradigms']
 	computer = ['Cache','API','Mainframe','Firewall','Server','Cloud','RAM','Online', 'Sentiment Analysis','Algorithm']
 	company = ['.com','Enterprises','Ltd.','International','Incorporated','Consolidated', 'Global']
-	output = [ buzzadj[randint(0,len (buzzadj) -1)] + ' ' + company[randint(0,len (company) -1)], buzzadj[randint(0,len (buzzadj) -1)] + ' ' +  buzznouns[randint(0,len (buzznouns ) -1)], computer[randint(0,len (computer) -1)] + ' ' + company[randint(0,len (company) -1)], buzzadj[randint(0,len (buzzadj) -1)] + ' ' +  computer[randint(0,len (computer) -1)] + ' ' +  company[randint(0,len (company) -1)], 'Advanced Virtual Lawnmowers']
-	return output[randint(0,len (output) -1)]
+	output = [ buzzadj[randint(0,len (buzzadj) -1)] + ' ' + company[randint(0,len (company) -1)], buzzadj[randint(0,len (buzzadj) -1)] + ' ' +  buzznouns[randint(0,len (buzznouns ) -1)], computer[randint(0,len (computer) -1)] + ' ' + company[randint(0,len (company) -1)], buzzadj[randint(0,len (buzzadj) -1)] + ' ' +  computer[randint(0,len (computer) -1)] + ' ' +  company[randint(0,len (company) -1)]]
+	return output[randint(0,len (output) -1)] if randint(0,100) else 'Advanced Virtual Lawnmowers'
 
 
 print startup_words()
