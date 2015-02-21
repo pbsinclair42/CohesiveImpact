b1e3f858bf0ddbd0e5e4150f06eb9e7924108d2e
A-W-Smith <aws2411@gmail.com>
2015-02-21 20:30:10 +0000

    Better Things

---
+++ name_gen.py
@@ -0,0 +1,5 @@
+def words():
+	buzzwords = ['grass-roots', 'synergy' , 'proactive','adhesive','cohesive','practical','globalization','robust','sustainabale','impact','functional','transformative','dynamic']
+	computer = ['cache','api','mainframe','firewall','server','cloud','RAM','online']
+	company = ['.com','enterprises','ltd','international','incorporated','consolidated']
+	
