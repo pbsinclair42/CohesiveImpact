70d09d973e322d51bc2ff12ade23bd7269e8ac8b
A-W-Smith <aws2411@gmail.com>
2015-02-21 21:29:03 +0000

    Damn

--- name_gen.py
+++ name_gen.py
@@ -1,11 +1,10 @@
 from random import randint
 
 def startup_words():
-	buzzadj = ['Grass-roots', 'Synergistic' , 'Proactive','Adhesive','Cohesive','Practical','Robust','Sustainabale','Functional','Transformative','Dynamic']
+	buzzadj = ['Grass-roots', 'Synergistic' , 'Proactive','Adhesive','Cohesive','Practical','Robust','Sustainabale','Functional','Transformative','Dynamic','Worldwide']
 	buzznouns = ['Synergy', 'Adhesion','Cohesion','Impact','Globalization']
-	computer = ['Cache','API','Mainframe','Firewall','Server','Cloud','RAM','Online']
-	company = ['.com','Enterprises','Ltd.','International','Incorporated','Consolidated', 'Global']
+	computer = ['Cache','API','Mainframe','Firewall','Server','Cloud','RAM','Online','Doge','Memes','Networking','Social Media']
+	company = ['.com','Enterprises','Ltd.','International','Incorporated','Consolidated', 'Global', 'Security','Technologies']
 	output = [ buzzadj[randint(0,len (buzzadj) -1)] + ' ' + company[randint(0,len (company) -1)], buzzadj[randint(0,len (buzzadj) -1)] + ' ' +  buzznouns[randint(0,len (buzznouns ) -1)], computer[randint(0,len (computer) -1)] + ' ' + company[randint(0,len (company) -1)], buzzadj[randint(0,len (buzzadj) -1)] + ' ' +  computer[randint(0,len (computer) -1)] + ' ' +  company[randint(0,len (company) -1)], 'Advanced Virtual Lawnmowers']
 	return output[randint(0,len (output) -1)]
-
 print startup_words()
