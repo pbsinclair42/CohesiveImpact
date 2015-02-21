bfc78f93a88904cc6fe584c4880c9651f0698cd6
Paul Sinclair <bobsmith0030+git2@gmail.com>
2015-02-21 21:19:20 +0000

    Added a few buzzwords

--- name_gen.py
+++ name_gen.py
@@ -1,9 +1,9 @@
 from random import randint
 
 def startup_words():
-	buzzadj = ['grass-roots', 'synergistic' , 'proactive','adhesive','cohesive','practical','robust','sustainabale','functional','transformative','dynamic']
-	buzznouns = ['synergy', 'adhesion','cohesion','impact','globalization']
-	computer = ['cache','api','mainframe','firewall','server','cloud','RAM','online']
+	buzzadj = ['grass-roots', 'synergistic' , 'proactive','adhesive','cohesive','practical','robust','sustainabale','functional','transformative','dynamic','Social','Crowdsourced']
+	buzznouns = ['synergy', 'adhesion','cohesion','impact','globalization','Solutions','Administration','Management','Paradigms']
+	computer = ['cache','api','mainframe','firewall','server','cloud','RAM','online', 'Sentiment Analysis','Algorithm']
 	company = ['.com','enterprises','ltd','international','incorporated','consolidated', 'global']
 	x = randint(0,3)
 	y = randint(0,6)
