c9e00c95a0fd2e748da0610a045a509d2e3df627
Paul Sinclair <bobsmith0030+git2@gmail.com>
2015-02-22 10:48:44 +0000

    Started trying to connect to Twitter

--- name_gen.py
+++ name_gen.py
@@ -4,9 +4,9 @@ def startup_words():
 
 	buzzadj = ['Grass-roots', 'Synergistic' , 'Proactive','Adhesive','Cohesive','Practical','Robust','Sustainabale','Functional','Transformative','Dynamic','Social','Crowdsourced','Inspirational','Innovative','Strategic','World Class','Active','Holistic','World Wide','Game Changing','Professional']
 	buzznouns = ['Synergy', 'Adhesion','Cohesion','Impact','Globalization','Solutions','Administration','Management','Paradigms','Social Media','Innovation']
-	computer = ['Cache','API','Mainframe','Firewall','Server','Cloud','RAM','Online', 'Sentiment Analysis','Algorithm','Virtual Reality','Multimedia','Data Visualisation','GUI','User Experience','Bootstrap','Technology','CPU','Motherboard','Parallel','Agile','Semantics']
+	computer = ['Cache','API','Mainframe','Firewall','Server','Cloud','RAM','Online', 'Sentiment Analysis','Algorithm','Virtual Reality','Multimedia','Data Visualisation','GUI','User Experience','Bootstrap','Technology','CPU','Motherboard','Parallel','Semantics']
 	company = ['.com','Enterprises','Ltd.','International','Incorporated','Consolidated', 'Solutions','Association','Corporation','Foundation','Institute','Society','Applications','Syndicate','Cooperative']
-	output = [ buzzadj[randint(0,len (buzzadj) -1)] + ' '+  buzznouns[randint(0,len (buzznouns ) -1)]+' '+ company[randint(0,len (company) -1)], buzzadj[randint(0,len (buzzadj) -1)] + ' ' +  computer[randint(0,len (computer) -1)] + ' ' +  company[randint(0,len (company) -1)]]
+	output = [ buzzadj[randint(0,len (buzzadj) -1)] + ' '+  buzznouns[randint(0,len (buzznouns ) -1)]+' '+ company[randint(0,len (company) -1)], buzzadj[randint(0,len (buzzadj) -1)] + ' ' +  computer[randint(0,len (computer) -1)] + ' ' +  company[randint(0,len (company) -1)], buzzadj[randint(0,len (buzzadj) -1)] + ' ' +  computer[randint(0,len (computer) -1)] ]
 	if randint(0,100)==100:
 		return 'Advanced Virtual Lawnmowers'
 	toReturn = output[randint(0,len (output) -1)] 
@@ -14,4 +14,13 @@ def startup_words():
 		toReturn=toReturn.replace(" ","")
 	return toReturn
 
+print startup_words()
+print startup_words()
+print startup_words()
+print startup_words()
+print startup_words()
+print startup_words()
+print startup_words()
+print startup_words()
+print startup_words()
 print startup_words()
---
+++ twitterTest.py
@@ -0,0 +1,12 @@
+'''from birdy.twitter import UserClient'''
+import tweepy
+from keys import keys
+
+CONSUMER_KEY = keys['consumer_key']
+CONSUMER_SECRET = keys['consumer_secret']
+ACCESS_TOKEN = keys['access_token']
+ACCESS_TOKEN_SECRET = keys['access_token_secret']
+ 
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
