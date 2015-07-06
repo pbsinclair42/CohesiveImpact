07c5c57e83f7ad10af6379aa1bb330e641197783
Paul Sinclair <bobsmith0030+git2@gmail.com>
2015-07-06 11:52:36 +0100

    Added more buzzwords

--- name_gen.py
+++ name_gen.py
@@ -2,9 +2,9 @@ from random import randint
 
 def startup_words():
 
-	buzzadj = ['Grass-roots', 'Synergistic' , 'Proactive','Adhesive','Cohesive','Practical','Robust','Sustainabale','Functional','Transformative','Dynamic','Social','Crowdsourced','Inspirational','Innovative','Strategic','World Class','Active','Holistic','World Wide','Game Changing','Professional']
-	buzznouns = ['Synergy', 'Adhesion','Cohesion','Impact','Globalization','Solutions','Administration','Management','Paradigms','Social Media','Innovation','Linkativity']
-	computer = ['Cache','API','Mainframe','Firewall','Server','Cloud','RAM','Online', 'Sentiment Analysis','Algorithm','Virtual Reality','Multimedia','Data Visualisation','GUI','User Experience','Bootstrap','Technology','CPU','Motherboard','Parallel','Semantics']
+	buzzadj = ['Grass-roots', 'Synergistic' , 'Proactive','Adhesive','Cohesive','Practical','Robust','Sustainabale','Functional','Transformative','Dynamic','Social','Crowdsourced','Inspirational','Innovative','Strategic','World Class','Active','Holistic','World Wide','Game Changing','Professional','Effective']
+	buzznouns = ['Synergy', 'Adhesion','Cohesion','Impact','Globalization','Solutions','Administration','Management','Paradigms','Social Media','Innovation','Linkativity','Boundarylessness','Collaboration','Networking','Professionalism','Intersubjectivity']
+	computer = ['Cache','APIs','Mainframes','Firewalls','Servers','Cloud','RAM', 'Sentiment Analysis','Algorithms','Virtual Reality','Multimedia','Data Visualisation','GUI','User Experience','Bootstrapping','Technology','CPUs','Motherboards','Parallels','Semantics','Scripting']
 	company = ['.com','Enterprises','Ltd.','International','Incorporated','Consolidated', 'Solutions','Association','Corporation','Foundation','Institute','Society','Applications','Syndicate','Cooperative']
 	output = [ buzzadj[randint(0,len (buzzadj) -1)] + ' '+  buzznouns[randint(0,len (buzznouns ) -1)]+' '+ company[randint(0,len (company) -1)], buzzadj[randint(0,len (buzzadj) -1)] + ' ' +  computer[randint(0,len (computer) -1)] + ' ' +  company[randint(0,len (company) -1)], buzzadj[randint(0,len (buzzadj) -1)] + ' ' +  computer[randint(0,len (computer) -1)] ]
 	if randint(0,100)==100:
