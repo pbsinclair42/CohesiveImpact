8e4c0011ce9755664effec50f0f600678290ac7b
Paul Sinclair <bobsmith0030+git2@gmail.com>
2015-07-06 12:13:51 +0100

    Added more buzzwords

--- name_gen.py
+++ name_gen.py
@@ -2,8 +2,8 @@ from random import randint
 
 def startup_words():
 
-	buzzadj = ['Grass-roots', 'Synergistic' , 'Proactive','Adhesive','Cohesive','Practical','Robust','Sustainabale','Functional','Transformative','Dynamic','Social','Crowdsourced','Inspirational','Innovative','Strategic','World Class','Active','Holistic','World Wide','Game Changing','Professional','Effective']
-	buzznouns = ['Synergy', 'Adhesion','Cohesion','Impact','Globalization','Solutions','Administration','Management','Paradigms','Social Media','Innovation','Linkativity','Boundarylessness','Collaboration','Networking','Professionalism','Intersubjectivity']
+	buzzadj = ['Grass-roots', 'Synergistic' , 'Proactive','Adhesive','Cohesive','Practical','Robust','Sustainabale','Functional','Transformative','Dynamic','Social','Crowdsourced','Inspirational','Innovative','Strategic','World Class','Active','Holistic','World Wide','Game Changing','Professional','Effective','Maximum Value']
+	buzznouns = ['Synergy', 'Adhesion','Cohesion','Impact','Globalization','Solutions','Administration','Management','Paradigms','Social Media','Innovation','Linkativity','Boundarylessness','Collaboration','Networking','Professionalism','Intersubjectivity','Performativity']
 	computer = ['Cache','APIs','Mainframes','Firewalls','Servers','Cloud','RAM', 'Sentiment Analysis','Algorithms','Virtual Reality','Multimedia','Data Visualisation','GUI','User Experience','Bootstrapping','Technology','CPUs','Motherboards','Parallels','Semantics','Scripting']
 	company = ['.com','Enterprises','Ltd.','International','Incorporated','Consolidated', 'Solutions','Association','Corporation','Foundation','Institute','Society','Applications','Syndicate','Cooperative']
 	output = [ buzzadj[randint(0,len (buzzadj) -1)] + ' '+  buzznouns[randint(0,len (buzznouns ) -1)]+' '+ company[randint(0,len (company) -1)], buzzadj[randint(0,len (buzzadj) -1)] + ' ' +  computer[randint(0,len (computer) -1)] + ' ' +  company[randint(0,len (company) -1)], buzzadj[randint(0,len (buzzadj) -1)] + ' ' +  computer[randint(0,len (computer) -1)] ]
